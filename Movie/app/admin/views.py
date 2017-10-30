9b22516d97542baa0efd1c1774f7eac144e2e3aa
fbb <1787274712@qq.com>
2017-10-30 19:56:21 +0800

    add templates

--- Movie/app/admin/views.py
+++ Movie/app/admin/views.py
@@ -1,5 +1,94 @@
+from flask import render_template,redirect,url_for
 from . import admin
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:green'>this is admin</h1>"
+    return render_template('admin/index.html')
+
+@admin.route("/login/")
+def login():
+    return render_template('admin/login.html')
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for('admin.login'))
+
+@admin.route("/change_pwd/")
+def change_pwd():
+    return render_template('admin/change_pwd.html')
+
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template('admin/tag_add.html')
+
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template('admin/tag_list.html')
+
+@admin.route("/movie/add/")
+def movie_add():
+    return render_template('admin/movie_add.html')
+
+@admin.route("/movie/list/")
+def movie_list():
+    return render_template('admin/movie_list.html')
+
+@admin.route("/preview/add/")
+def preview_add():
+    return render_template('admin/preview_add.html')
+
+@admin.route("/preview/list/")
+def preview_list():
+    return render_template('admin/preview_list.html')
+
+@admin.route("/user/list/")
+def user_list():
+    return render_template('admin/user_list.html')
+
+@admin.route("/user/info/")
+def user_info():
+    return render_template('admin/user_info.html')
+
+@admin.route("/comment/list/")
+def comment_list():
+    return render_template('admin/comment_list.html')
+
+@admin.route("/movie_col/list/")
+def movie_col_list():
+    return render_template('admin/movie_col_list.html')
+
+@admin.route("/operation_log/list/")
+def operation_log_list():
+    return render_template('admin/operation_log_list.html')
+
+@admin.route("/admin_loginlog/list/")
+def admin_loginlog_list():
+    return render_template('admin/admin_loginlog_list.html')
+
+@admin.route("/user_loginlog/list/")
+def user_loginlog_list():
+    return render_template('admin/user_loginlog_list.html')
+
+@admin.route("/role/add/")
+def role_add():
+    return render_template('admin/role_add.html')
+
+@admin.route("/role/list/")
+def role_list():
+    return render_template('admin/role_list.html')
+
+@admin.route("/auth/add/")
+def auth_add():
+    return render_template('admin/auth_add.html')
+
+@admin.route("/auth/list/")
+def auth_list():
+    return render_template('admin/auth_list.html')
+
+@admin.route("/admin/add/")
+def admin_add():
+    return render_template('admin/admin_add.html')
+
+@admin.route("/admin/list/")
+def admin_list():
+    return render_template('admin/admin_list.html')
--- Movie/app/home/views.py
+++ Movie/app/home/views.py
@@ -1,5 +1,56 @@
+from flask import render_template,redirect,url_for
 from . import home
 
-@home.route("/")
+@home.route('/')
 def index():
-    return "<h1 style='color:red'>this is home</h1>"
+    return render_template('home/index.html')
+
+@home.route('/login/')
+def login():
+    return render_template('home/login.html')
+
+@home.route('/logout/')
+def logout():
+    return redirect(url_for('home.login'))
+
+@home.route('/register/')
+def register():
+    return render_template('home/register.html')
+
+
+@home.route('/user_info/')
+def user_info():
+    return render_template('home/user_info.html')
+
+
+@home.route('/change_pwd/')
+def change_pwd():
+    return render_template('home/change_pwd.html')
+
+
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+
+@home.route('/login_log/')
+def login_log():
+    return render_template('home/login_log.html')
+
+
+@home.route('/movie_col/')
+def movie_col():
+    return render_template('home/movie_col.html')
+
+@home.route('/animation/')
+def animation():
+    return render_template('home/animation.html')
+
+@home.route('/search/')
+def search():
+    return render_template('home/search.html')
+
+@home.route('/play/')
+def play():
+    return render_template('home/play.html')
+
